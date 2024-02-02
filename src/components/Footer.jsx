bb36e95772ba78fc5c42f6169839dd49a7aeca9e
Gbenghar <olatunjigbenga21@gmail.com>
2024-02-02 12:57:35 +0100

    logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 
 import MailChimpForm  from "../components/MailChimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
