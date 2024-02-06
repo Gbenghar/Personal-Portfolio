f524b9e3865d3857670226486a79381ea03480a4
Gbenga Olatunji <145749533+Gbenghar@users.noreply.github.com>
2024-02-06 18:14:44 +0100

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,12 +5,13 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <a href={link} target="_blank"></a>
       <div className="proj-imgbx">
         <img src={imgUrl} alt="imgurl" />
 
         <div className="proj-txtx">
-          <h4>{title}</h4>
+          <h4>
+            <span>{title} <a href={link} target="_blank"></a></span>
+          </h4>
           <span>{description}</span>
         </div>
       </div>
