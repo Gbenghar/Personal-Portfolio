ad3482304e8602f4ee7adf121f566f6a1fcfc2bf
Gbenga Olatunji <145749533+Gbenghar@users.noreply.github.com>
2024-02-06 18:08:03 +0100

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,9 +6,8 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <a href={link} target="_blank">
-          <img src={imgUrl} alt="imgurl" />
-        </a>
+        <a href={link} target="_blank"></a>
+        <img src={imgUrl} alt="imgurl" />
 
         <div className="proj-txtx">
           <h4>{title}</h4>
